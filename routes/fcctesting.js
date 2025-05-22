730889231786fa8b0ea81132ae885e4829173ee6
Singor.KPATCHOU <skpatchou@hyperion-dev.com>
2025-05-22 14:54:12 +0200

    test

--- routes/fcctesting.js
+++ routes/fcctesting.js
@@ -10,14 +10,20 @@ module.exports = function(app) {
     .get(function(req, res) {
       console.log('GET /_api/get-tests');
       res.json([
-        {
-          title: 'Test 1',
-          message: 'Test 1 message'
-        },
-        {
-          title: 'Test 2',
-          message: 'Test 2 message'
-        }
+        { title: 'Test 1', message: 'Test 1 message' },
+        { title: 'Test 2', message: 'Test 2 message' },
+        { title: 'Test 3', message: 'Test 3 message' },
+        { title: 'Test 4', message: 'Test 4 message' },
+        { title: 'Test 5', message: 'Test 5 message' },
+        { title: 'Test 6', message: 'Test 6 message' },
+        { title: 'Test 7', message: 'Test 7 message' },
+        { title: 'Test 8', message: 'Test 8 message' },
+        { title: 'Test 9', message: 'Test 9 message' },
+        { title: 'Test 10', message: 'Test 10 message' },
+        { title: 'Test 11', message: 'Test 11 message' },
+        { title: 'Test 12', message: 'Test 12 message' },
+        { title: 'Test 13', message: 'Test 13 message' },
+        { title: 'Test 14', message: 'Test 14 message' }
       ]);
     });
 
