ab7b46c642546f7228fecda29e7a3fd0834b66df
Singor.KPATCHOU <skpatchou@hyperion-dev.com>
2025-05-22 14:52:00 +0200

    test

--- routes/fcctesting.js
+++ routes/fcctesting.js
@@ -9,18 +9,16 @@ module.exports = function(app) {
   app.route('/_api/get-tests')
     .get(function(req, res) {
       console.log('GET /_api/get-tests');
-      res.json({
-        tests: [
-          {
-            title: 'Test 1',
-            message: 'Test 1 message'
-          },
-          {
-            title: 'Test 2',
-            message: 'Test 2 message'
-          }
-        ]
-      });
+      res.json([
+        {
+          title: 'Test 1',
+          message: 'Test 1 message'
+        },
+        {
+          title: 'Test 2',
+          message: 'Test 2 message'
+        }
+      ]);
     });
 
   // Endpoint pour récupérer le code source du serveur
