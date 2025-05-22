1ba390028dc21768db60ddf3e86db35fa4d5afb6
Singor.KPATCHOU <skpatchou@hyperion-dev.com>
2025-05-22 14:32:28 +0200

    test

--- routes/fcctesting.js
+++ routes/fcctesting.js
@@ -2,6 +2,40 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
+
+module.exports = function(app) {
+  // Endpoint pour les tests FreeCodeCamp
+  app.route('/_api/get-tests')
+    .get(function(req, res) {
+      console.log('GET /_api/get-tests');
+      res.json({
+        tests: [
+          {
+            title: 'Test 1',
+            message: 'Test 1 message'
+          },
+          {
+            title: 'Test 2',
+            message: 'Test 2 message'
+          }
+        ]
+      });
+    });
+
+  // Endpoint existant pour récupérer le code source du serveur
+  app.route('/_api/server.js')
+    .get(function(req, res, next) {
+      console.log('GET /_api/server.js');
+      fs.readFile(path.join(process.cwd(), 'server.js'), function(err, data) {
+        if (err) return next(err);
+        res.type('txt').send(data.toString());
+      });
+    });
+};// routes/fcctesting.js
+'use strict';
+
+const fs = require('fs');
 
 module.exports = function(app) {
   app.route('/_api/server.js')
